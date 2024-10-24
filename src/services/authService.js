8ed09993311b8b7b0e86e0b62caa0e312a3e8ade
Vigneshwarrs <vigneshwarrs@outlook.com>
2024-10-24 10:29:06 +0530

    Updated

--- src/services/authService.js
+++ src/services/authService.js
@@ -5,21 +5,21 @@ import { baseURL } from '../utils/api';
 // const API_URL = "http://localhost:5000/api/auth/";
 
 export const signIn = (email, password) => {
-  return axios.post(`${baseURL}/sign-in`, { email, password });
+  return axios.post(`${baseURL}/auth/sign-in`, { email, password });
 };
 
 export const signUp = (data) => {
-  return axios.post(`${baseURL}/sign-up`, data);
+  return axios.post(`${baseURL}/auth/sign-up`, data);
 };
 
 export const forgotPassword = (email) => {
-  return axios.post(`${baseURL}/forgot-password`, email);
+  return axios.post(`${baseURL}/auth/forgot-password`, email);
 };
 
 export const resetPassword = (token, password) => {
-  return axios.post(`${baseURL}/reset-password/${token}`, password );
+  return axios.post(`${baseURL}/auth/reset-password/${token}`, password );
 };
 
 export const activateUser = (token) => {
-  return axios.get(`${baseURL}/activate/${token}`);
+  return axios.get(`${baseURL}/auth/activate/${token}`);
 }
